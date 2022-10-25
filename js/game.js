f317b7725808b9c90196ea6ba82e5a21ac45bcd1
James Mayr <jamesbmayr@gmail.com>
2022-10-24 20:39:15 -0400

    adjust pingloop

--- js/game.js
+++ js/game.js
@@ -151,6 +151,7 @@ window.addEventListener("load", function() {
 			function createSocket() {
 				try {
 					SOCKET = new WebSocket(location.href.replace("http","ws"))
+					SOCKET.keepPinging = false
 					SOCKET.onopen = function() {
 						SOCKET.send(null)
 					}
@@ -164,6 +165,7 @@ window.addEventListener("load", function() {
 					}
 					SOCKET.onmessage = function(message) {
 						try {
+							SOCKET.keepPinging = true
 							var post = JSON.parse(message.data)
 							if (post && (typeof post == "object")) {
 								receiveSocket(post)
@@ -176,9 +178,12 @@ window.addEventListener("load", function() {
 						clearInterval(SOCKET.pingLoop)
 					}
 					SOCKET.pingLoop = setInterval(function() {
-						fetch("/ping", {method: "GET"})
-							.then(function(response){ return response.json() })
-							.then(function(data) {})
+						if (SOCKET.keepPinging) {
+							SOCKET.keepPinging = false
+							fetch("/ping", {method: "GET"})
+								.then(function(response){ return response.json() })
+								.then(function(data) {})
+						}
 					}, PINGINTERVAL)
 				}
 				catch (error) {console.log(error)}
